3bf5e0a124cd7da6d97d8d9931ce7f1d8d9b8e19
ravi <raviamma65@gmail.com>
2024-07-15 11:35:21 +0000

    new project

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+}
+
+resource "aws_instance" "one" {
+ami = "ami-0c9c2640236f1c5f3"
+instance_type = "t2.micro"
+count = 1
+tags = {
+Name = "Web-server"
+}
+}
+
+output { 
+value = aws_instance.one[*].public_ip
+
